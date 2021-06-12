28b505cf7e4ab17ced708fcad129c3eab15d5b0c
RadomirV <rimodar2809@gmail.com>
2021-06-12 19:38:28 +0600

    commit

---
+++ haffman.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <chrono>
+#include <time.h>
+#include <cstring>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream graph("C:\\Users\\12\\Desktop\\c_labs\\My_files\\Graf.txt");
+    string s;//[10000];
+    int number_of_vertex = 0;
+    int start = 0, end = 0;
+    graph >> s;
+    //sgfd
+    cout << s << endl;
+    int ix = 0;
+    //while (s[ix] != ',')
+    return 0;
+}
