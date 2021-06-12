a2096093c2badf7c54148f821499bed4f52b6e25
RadomirV <rimodar2809@gmail.com>
2021-06-12 19:59:25 +0600

    4

--- haffman.cpp
+++ haffman.cpp
@@ -10,13 +10,14 @@ using namespace std;
 int main()
 {
     ifstream graph("C:\\Users\\12\\Desktop\\c_labs\\My_files\\Graf.txt");
-    string s;//[10000];
+    string s; //[10000];
     int number_of_vertex = 0;
     int start = 0, end = 0;
     graph >> s;
     //sgfd
     cout << s << endl;
     int ix = 0;
+    int werew = 0;
     //comcommit 1
     //while (s[ix] != ',')
 
