8cb9b076b59bb3f13e424c071c86b6f4d8a2bbf5
RadomirV <rimodar2809@gmail.com>
2021-07-03 01:18:14 +0600

    Finish

--- haffman.cpp
+++ haffman.cpp
@@ -203,7 +203,7 @@ bool Decode_haffman(char *codedway, char *decodedway)
     {
         coded.read((char *)&s, sizeof(s));
         coded.read((char *)&sym_freq, sizeof(sym_freq));
-        --n;
+        n--;
         table[s] = sym_freq;
     }
 
@@ -319,39 +319,3 @@ int main()
 
     return 0;
 }
-/*
-int main()
-{
-
-    ifstream text;
-    text.open("C:\\Users\\12\\Desktop\\c_labs\\haffman\\text.txt");
-    int ix = 0;
-    int werew = 0;
-    int text_size = 0;
-    while (!text.eof())
-    {
-        text_size++;
-        text.get();
-    }
-
-    unsigned char str[text_size];
-    for (int i = 0; i < text_size; i++)
-        str[i] = 0;
-    text >> str;
-
-    for (int i = 0; i < text_size; i++)
-        cout << str[i];
-
-    /*while (!text.eof())
-    {
-        s = text.get();
-        cout << s[ix];
-        ix++;
-    }
-    //comcommit 1
-    //while (s[ix] != ',')
-    //git commit 20:02
-
-    return 0;
-}
-*/
